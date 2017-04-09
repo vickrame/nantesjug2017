853ff7f5b7c2924c4f800b9302213d2c56837ae7
vickrame <ujoodha.vickrame@gmail.com>
2017-04-09 20:26:12 +0200

    suppression code en dur

--- workspace/hystrix/src/main/java/com/clea/nantes/jug/netflix/hystrix/command/PersonnagesServiceCommand.java
+++ workspace/hystrix/src/main/java/com/clea/nantes/jug/netflix/hystrix/command/PersonnagesServiceCommand.java
@@ -1,8 +1,12 @@
 package com.clea.nantes.jug.netflix.hystrix.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
@@ -10,13 +14,19 @@ public class PersonnagesServiceCommand {
 
 	private RestTemplate rt;
 
+	@Autowired
+	private EurekaClient eurekaClient;
 
 	@HystrixCommand(fallbackMethod = "annuairePersonnagesIndisponible")
 	public String statusAnnuairePersonne() {
-		String url = "http://localhost:8061";
-		String ret = "";
-		rt = new RestTemplate();
-		ret = rt.getForObject(url+"/info", String.class);
+		InstanceInfo  instanceInfo = eurekaClient.getNextServerFromEureka("annuaire-personne", false);
+		rt= new RestTemplate();
+		
+		ResponseEntity<String> responseHttp = rt.getForEntity(instanceInfo.getHomePageUrl()+"/info", String.class);
+		String ret=null;
+		if(responseHttp.getStatusCode().is2xxSuccessful()){
+			ret = responseHttp.getBody();
+		}
 		return ret;
 
 	}
